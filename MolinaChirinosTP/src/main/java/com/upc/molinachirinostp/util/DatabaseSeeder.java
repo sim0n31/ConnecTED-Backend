c505d3030b2ee43a5205a0c397896b315694dae2
Simón Molina <u202312714@upc.edu.pe>
2025-11-28 06:21:33 -0500

    Quinta

--- MolinaChirinosTP/src/main/java/com/upc/molinachirinostp/util/DatabaseSeeder.java
+++ MolinaChirinosTP/src/main/java/com/upc/molinachirinostp/util/DatabaseSeeder.java
@@ -14,7 +14,7 @@ import java.nio.file.Paths;
 import java.util.stream.Collectors;
 
 @Component
-@Profile("!test")
+@Profile("dev")
 public class DatabaseSeeder implements CommandLineRunner {
 
     private final JdbcTemplate jdbcTemplate;
